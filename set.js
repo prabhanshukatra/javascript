0c02e57b14daba0fbc7459843c2e13c02be2b43f
prabhanshukatra <katraprabhasnhu02@gmail.com>
2024-11-21 21:52:56 +0530

    hi

---
+++ set.js
@@ -0,0 +1,12 @@
+document.getElementById("a").innerHTML = "<center><h2>Prabhnshu katra</h2></center>";
+
+document.getElementById("a").setAttribute ("style", "border: 10px dotted red");
+
+document.getElementById("a").removeAttribute ("style");
+
+document.getElementById("b").innerHTML = "<h1>Gwalior</h1>";
+
+// querySelector
+document.querySelector("#a").innerHTML = "<center><h2>Prabhnshu 2katra</h2></center>";
+
+document.querySelector('.c').innerHTML = "<h1>Gwalior</h1>";
